48b92fd16504b31f00d944cd47076de6fddbcf62
ark948 <armankarimi948@gmail.com>
2025-03-08 14:49:30 +0330

    Search function added, no modifier yet

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 
 # Create your views here.
+
 
 @login_required
 def index(request):
     contacts = request.user.contacts.all().order_by('-created_at') # minus means descending
     context = {'contacts': contacts}
     return render(request, 'contacts.html', context)
+
+
+
+@login_required
+def search_contacts(request):
+    query = request.GET.get('search', "") # name of the search input field, default to empty if not provided
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    )
+    return render(request, 'partials/contact_list.html', {"contacts": contacts})
