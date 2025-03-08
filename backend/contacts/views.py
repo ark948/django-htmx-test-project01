c8276275605eb70d9164ca32fbe98614a3e5e81b
ark948 <armankarimi948@gmail.com>
2025-03-08 14:12:20 +0330

    Added user's contact list

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
-    context = {}
+    contacts = request.user.contacts.all().order_by('-created_at') # minus means descending
+    context = {'contacts': contacts}
     return render(request, 'contacts.html', context)
