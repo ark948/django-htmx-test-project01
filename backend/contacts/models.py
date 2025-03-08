dd4283b89f788437a1eba7fc9648f1e03dd7b69b
ark948 <armankarimi948@gmail.com>
2025-03-08 13:51:21 +0330

    Added contact model

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -7,3 +7,19 @@ class User(AbstractUser):
 
 
 
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='contacts'  # user.contacts.all()
+    )
+
+    class Meta:
+        unique_together = ('user', 'email')
+        # this constraint will prevent the addition of an object with the same user and email address
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
